c1ab75917c6fe73b96f662438f7f0dca16ec2073
rstrudel <rstrudel@gmail.com>
2019-11-12 16:16:44 +0100

    Set default data directory

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 # Training settings
 parser = argparse.ArgumentParser(description='RecVis A3 training script')
-parser.add_argument('--data', type=str, default='/media/hdd/bird_dataset', metavar='D',
+parser.add_argument('--data', type=str, default='bird_dataset', metavar='D',
                     help="folder where data is located. train_images/ and val_images/ need to be found in the folder")
 parser.add_argument('--batch-size', type=int, default=64, metavar='B',
                     help='input batch size for training (default: 64)')
